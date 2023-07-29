bd797ce7a4a84d2b6c1f71b045454bf66dd8e49d
MakAlijevic <alijevic.mak@gmail.com>
2023-07-30 00:23:18 +0200

    added userDb connection

---
+++ API/SocialMediaAPI/SocialMediaAPI.DAL/Data/UserDataContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SocialMediaAPI.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialMediaAPI.DAL.Data
+{
+    public class UserDataContext : DbContext
+    {
+        public UserDataContext(DbContextOptions<UserDataContext> options) : base(options) 
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
--- API/SocialMediaAPI/SocialMediaAPI/Program.cs
+++ API/SocialMediaAPI/SocialMediaAPI/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using SocialMediaAPI.DAL.Data;
+
 namespace SocialMediaAPI
 {
     public class Program
@@ -8,6 +11,9 @@ namespace SocialMediaAPI
 
             // Add services to the container.
 
+            builder.Services.AddDbContext<UserDataContext>(options => {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("UserDBDefaultConnection"));
+            });
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
