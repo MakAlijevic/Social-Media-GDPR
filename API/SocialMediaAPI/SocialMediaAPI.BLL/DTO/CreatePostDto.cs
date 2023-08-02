8567f66d8583eebd70c5e842251636346c26ff3c
emina <emina.sirbubalo@stu.ibu.edu.ba>
2023-08-02 22:37:11 +0200

    Created add posts logic

---
+++ API/SocialMediaAPI/SocialMediaAPI.BLL/DTO/CreatePostDto.cs
@@ -0,0 +1,15 @@
+﻿using SocialMediaAPI.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialMediaAPI.BLL.DTO
+{
+    public class CreatePostDto
+    {
+        public Guid Author { get; set; }
+        public string Content { get; set; }
+    }
+}
---
+++ API/SocialMediaAPI/SocialMediaAPI.BLL/Services/PostService.cs
@@ -0,0 +1,41 @@
+﻿using SocialMediaAPI.BLL.DTO;
+using SocialMediaAPI.BLL.Interface;
+using SocialMediaAPI.DAL.Interface;
+using SocialMediaAPI.DAL.Models;
+using SocialMediaAPI.DAL.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialMediaAPI.BLL.Services
+{
+    public class PostService:IPostService
+    {
+        private IPostRepository postRepository;
+        private IUserRepository userRepository;
+        public PostService(IPostRepository postRepository, IUserRepository userRepository)
+        {
+            this.postRepository = postRepository;
+            this.userRepository = userRepository;
+        }
+
+        public async Task<Post> AddPost(CreatePostDto postDto)
+        {
+            var author = await userRepository.GetUserById(postDto.Author);
+            if (author == null)
+            {
+                throw new Exception("Author doesnt exits");
+            }
+            var post = new Post
+            {
+                Author = postDto.Author,
+                Content = postDto.Content,
+                CreatedAt = DateTime.Now
+            };
+
+            return await postRepository.AddPost(post);
+        }
+    }
+}
